b5579fab4bb8336a8781c6a18518eb6186de6fdd
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-12-26 14:42:23 +0800

    update: complete buildTree

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 // Generate random sorted unique set numbers of array.
-const randomArray = Array.from({length: 20}, () => Math.floor(Math.random() * 40));
-const uniq = [...new Set(randomArray)];
-const sortArray = (array) => array.sort((a, b) => a - b);
-const sortedArray = sortArray(uniq);
-console.log(sortedArray)
+function generateArray() {
+  const sort = (a, b) => a - b;
+  const randomArray = Array.from({length: 20}, () => Math.floor(Math.random() * 40));
+  const uniq = [...new Set(randomArray)];
+  return uniq.sort();
+}
+
+class Node {
+    constructor(data, leftNode = null, rightNode = null) {
+        this.data = data;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+}
+
+class Tree {
+    constructor(array) {
+        this.root = this.buildTree(array, 0, array.length - 1);
+    }
+    buildTree(array) {
+        if (array.length === 1) return new Node(array[0]);
+        if (array.length === 0) return null;
+
+        let mid = Math.floor(array.length / 2);
+        let root = new Node(array[mid]);
+
+        root.leftNode = this.buildTree(array.slice(0, mid));
+        root.rightNode = this.buildTree(array.slice(mid + 1, array.length - 1));
+
+        return root;
+    }
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.rightNode !== null) {
+      prettyPrint(node.rightNode, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.leftNode !== null) {
+      prettyPrint(node.leftNode, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  };
+const sortedArray = generateArray();
+const tree = new Tree(sortedArray);
+prettyPrint(tree.root);
